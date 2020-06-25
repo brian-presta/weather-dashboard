ab29a6ac468927f44daa9d6d6ba3e1de73c330ea
brian presta <brian.presta@gmail.comgit commit -m first>
2020-06-24 19:00:22 -0700

    set up function to pass geo info into

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,33 @@
 var geoEncode = function(city) {
+    // the API endpoint we want to use to fetch actual weather data only takes latitude and longitude as search parameters,
+    // so we're only using this one as a geo encoder
     apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d1cd3159572faa76d674791448dcb10b`
     console.log(apiUrl)
     fetch(apiUrl)
     .then(function(response){
         if (response.ok) {
             response.json().then(function(data){
-                var geoInfo = {
+                var geoData = {
                     name:data.name,
                     lat:data.coord.lat,
                     lon:data.coord.lon
                 }
-                console.log(geoInfo)
+                getWeatherData(geoData)
+            })
+        }
+        else {
+            alert("Unable to perform search")
+        }
+    })
+}
+var getWeatherData = function(geoData) {
+    console.log(geoData)
+    apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${geoData.lat}&lon=${geoData.lon}&exclude=minutely,hourly&appid=d1cd3159572faa76d674791448dcb10b`
+    fetch(apiUrl)
+    .then(function(response){
+        if (response.ok) {
+            response.json().then(function(data){
+                console.log(data)
             })
         }
         else {
